47f1180d10a0beab8da8495bb23f509c61de20f2
William Lima <william.lima@objectedge.com>
2015-12-01 01:26:14 -0200

    Viewport settup

--- js/jGame.new.js
+++ js/jGame.new.js
@@ -117,6 +117,17 @@ function Jgame( config ) {
   function Room() {
     this.id = null;
     this.name = null;
+    this.dimensions = { width: 640, height: 480 };
+    this.viewports = [];
+    /* viewport settings */
+    this.add_viewport = function( options ) {
+      var def = { width: this.dimensions.width, height: this.dimensions.height, x: 0, y: 0 };
+      for(var key in options){
+        if (def.hasOwnProperty(key)) def[key] = options[key];
+      }
+      var index = this.viewports.push( def );
+      return this.viewports[ index - 1 ]
+    }
     this.instances = [];
     /* room function */
     this.instance_create = function( obj ) {
@@ -124,6 +135,8 @@ function Jgame( config ) {
       var index = this.instances.push( new_instance );
       return this.instances[ index - 1 ];
     }
+    /* add a default viewport */
+    this.add_viewport();
     /* return room object */
     return this;
   }
